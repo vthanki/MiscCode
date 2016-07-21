34150b992f2d9473f5e7e339aea4105d4e083296
Vishal Thanki <vishalthanki@gmail.com>
2016-07-21 21:57:28 +0200

    shell: Create partition using script
    Example script to create partitions and format them
    to vfat.
    
    Signed-off-by: Vishal Thanki <vishalthanki@gmail.com>

---
+++ shell/gen-part.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+if [ $# -eq 0 ];
+then
+	echo "Specify target disk"
+	exit
+fi
+
+
+TGTDEV=$1
+
+# to create the partitions programatically (rather than manually)
+# we're going to simulate the manual input to fdisk
+# The sed script strips off all the comments so that we can 
+# document what we're doing in-line with the actual commands
+# Note that a blank line (commented as "defualt" will send a empty
+# line terminated with a newline to take the fdisk default.
+
+# Source: http://superuser.com/questions/332252/creating-and-formating-a-partition-using-a-bash-script
+
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
+  o # clear the in memory partition table
+  n # new partition
+  p # primary partition
+  1 # partition number 1
+    # default - start at beginning of disk 
+  +500M # 500 MB boot parttion
+  n # new partition
+  p # primary partition
+  2 # partion number 2
+    # default, start immediately after preceding partition
+    # default, extend partition to end of disk
+  a # make a partition bootable
+  1 # bootable partition is partition 1 -- /dev/sda1
+  p # print the in-memory partition table
+  w # write the partition table
+  q # and we're done
+EOF
+
+sync
+
+mkfs.vfat $TGTDEV1
+sync
+mkfs.vfat $TGTDEV2
+sync
