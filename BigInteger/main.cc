0075f608c109878f3fd938e8b9ebe1da126bead0
Vishal Thanki <vishalthanki@gmail.com>
2017-01-08 18:52:11 +0100

    Add support for signed operation
    Adding sign support, and also some more refactoring to avoid the private
    method calls whenever possible to reduce stack usage.

--- BigInteger/main.cc
+++ BigInteger/main.cc
@@ -11,12 +11,13 @@ class BigInteger {
 private:
 	int arr[NR_DIGITS];
 	short int len;
+	bool isPositive;
 
 	void prependDigits(int digit);
 	void shrink();
 
 public:
-	BigInteger(): len(0) {
+	BigInteger(): len(0), isPositive(true) {
 		memset(arr, 0x0, sizeof(arr));
 	}
 	BigInteger(string digits, int base);
@@ -24,8 +25,6 @@ public:
 
 	void show() const;
 	short int getlen() const;
-
-	string getStringValue() const;
 
 	BigInteger operator +(BigInteger b) const;
 	BigInteger operator -(BigInteger b) const;
@@ -36,6 +35,7 @@ public:
 
 BigInteger::BigInteger(string digits, int base) {
 	memset(arr, 0x0, sizeof(arr));
+	isPositive = (digits[0] != '-');
 	len = digits.size();
 	//TODO: Throw exception in case of unsupported base
 	switch(base) {
@@ -53,6 +53,7 @@ BigInteger::BigInteger(int value) {
 	int i = NR_DIGITS - 1;
 	len = 0;
 	memset(arr, 0x0, sizeof(arr));
+	isPositive = (value >= 0);
 	do {
 		arr[i] = value % 10;
 		value /= 10;
@@ -70,11 +71,15 @@ void BigInteger::shrink() {
 	for (int i = 0; i < NR_DIGITS && !(arr[i]); i++)
 		lenToShrink++;
 
-	len -= lenToShrink;
-	if (!len) len = 1;
+	if (len > lenToShrink || lenToShrink == NR_DIGITS) {
+		len -= lenToShrink;
+		if (!len) len = 1;
+	}
 }
 
 void BigInteger::show() const {
+	if (!isPositive)
+		cout << "-";
 	for (int i = NR_DIGITS - len; i < NR_DIGITS; i++)
 		if (arr[i] >= 0)
 			cout << arr[i];
@@ -85,27 +90,19 @@ short int BigInteger::getlen() const {
 	return len;
 }
 
-
-string BigInteger::getStringValue() const {
-	string ret(len+1, 0);
-	for (int i = 0; i < len; i++)
-		ret[i] = '0' + arr[i];
-	return ret;
-}
-
 bool BigInteger::operator >(BigInteger b) const {
 
-	if (len > b.getlen())
+	if (len > b.len)
 		return true;
 
-	if (len == b.getlen() && arr[NR_DIGITS - len] > b.arr[NR_DIGITS - len])
+	if (len == b.len && arr[NR_DIGITS - len] > b.arr[NR_DIGITS - len])
 		return true;
 
 	return false;
 }
 
 bool BigInteger::operator ==(BigInteger b) const {
-	return getStringValue().compare(b.getStringValue()) == 0;
+	return (isPositive == b.isPositive) && !memcmp(arr, b.arr, sizeof(arr));
 }
 
 bool BigInteger::operator <(BigInteger b) const {
@@ -133,24 +130,31 @@ BigInteger BigInteger::operator +(BigInteger b) const {
 
 BigInteger BigInteger::operator -(BigInteger b) const {
 	BigInteger result;
-	int dig, carry = 0;
-	for (int i = NR_DIGITS - 1; i >= 0; i--) {
-		if ((arr[i] + carry) < b.arr[i]) {
-			dig = arr[i] + carry + 10 - b.arr[i];
-			carry = -1;
-		} else {
-			dig = arr[i] + carry - b.arr[i];
-			carry = 0;
+
+	if (*this < b) {
+		result = b - *this;
+		result.isPositive = false;
+	} else {
+		int dig, carry = 0;
+		for (int i = NR_DIGITS - 1; i >= 0; i--) {
+			if ((arr[i] + carry) < b.arr[i]) {
+				dig = arr[i] + carry + 10 - b.arr[i];
+				carry = -1;
+			} else {
+				dig = arr[i] + carry - b.arr[i];
+				carry = 0;
+			}
+			result.prependDigits(dig);
 		}
-		result.prependDigits(dig);
 	}
+
 	result.shrink();
 	return result;
 }
 
 int main(int argc, char *argv[]) {
-	BigInteger b1(50);
-	BigInteger b2 = 100;
+	BigInteger b1(498);
+	BigInteger b2 = 500;
 	BigInteger b3 = b1 - b2;
 	b3.show();
 	return 0;
