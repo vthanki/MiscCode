f8f6d71a2729818a4c4f05e5c35dc99a5070a38b
Vishal Thanki <vishalthanki@gmail.com>
2017-01-08 18:00:40 +0100

    Initial Commit
    The basic BigInteger class to handle integers which are big enough to be
    stored in >= 64 bit data types.

---
+++ BigInteger/main.cc
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+const int NR_DIGITS = 100;
+
+class BigInteger {
+private:
+	int arr[NR_DIGITS];
+	short int len;
+
+	void prependDigits(int digit);
+	int getDigitAt(int index) const;
+	void shrink();
+	void setLen(int len);
+
+public:
+	BigInteger(): len(0) {
+		memset(arr, 0x0, sizeof(arr));
+	}
+	//BigInteger(const char *value, int base);
+	BigInteger(string digits, int base);
+	BigInteger(int val);
+
+	void show() const;
+	short int getlen() const;
+
+	string getStringValue() const;
+
+	BigInteger operator +(BigInteger b) const;
+	BigInteger operator -(BigInteger b) const;
+	bool operator > (BigInteger b) const;
+	bool operator < (BigInteger b) const;
+	bool operator == (BigInteger b) const;
+	//string operator << (BigInteger b);
+};
+
+BigInteger::BigInteger(string digits, int base) {
+	memset(arr, 0x0, sizeof(arr));
+	len = digits.size();
+	//TODO: Throw exception in case of unsupported base
+	switch(base) {
+	case 10:
+		int i, j;
+		for (i = len - 1,j = NR_DIGITS-1; i >= 0; i--, j--) {
+			//TODO: Add sanity check for isDigit
+			arr[j] = digits[i] - '0';
+		}
+		break;
+	}
+}
+
+BigInteger::BigInteger(int value) {
+	int i = NR_DIGITS - 1;
+	len = 0;
+	memset(arr, 0x0, sizeof(arr));
+	do {
+		arr[i] = value % 10;
+		value /= 10;
+		i--;
+	} while (++len && value);
+}
+
+void BigInteger::prependDigits(int digit) {
+	arr[NR_DIGITS-1-len] = digit;
+	len++;
+}
+
+int BigInteger::getDigitAt(int index) const {
+	return arr[index];
+}
+
+void BigInteger::setLen(int newLen) {
+	if (newLen == 0)
+		newLen = 1;
+	len = newLen;
+}
+
+void BigInteger::shrink() {
+	int lenToShrink = 0;
+	for (int i = 0; i < NR_DIGITS && !(arr[i]); i++)
+		lenToShrink++;
+
+	setLen(len - lenToShrink);
+}
+
+void BigInteger::show() const {
+	for (int i = NR_DIGITS - len; i < NR_DIGITS; i++)
+		if (arr[i] >= 0)
+			cout << arr[i];
+	cout << endl;
+}
+
+short int BigInteger::getlen() const {
+	return len;
+}
+
+
+string BigInteger::getStringValue() const {
+	string ret(len+1, 0);
+	for (int i = 0; i < len; i++)
+		ret[i] = '0' + arr[i];
+	return ret;
+}
+
+bool BigInteger::operator >(BigInteger b) const {
+
+	if (len > b.getlen())
+		return true;
+
+	if (len == b.getlen() && arr[NR_DIGITS - len] > b.getDigitAt(NR_DIGITS - len))
+		return true;
+
+	return false;
+}
+
+bool BigInteger::operator ==(BigInteger b) const {
+	return getStringValue().compare(b.getStringValue()) == 0;
+}
+
+bool BigInteger::operator <(BigInteger b) const {
+	return !(*this > b) && !(*this == b);
+}
+
+BigInteger BigInteger::operator +(BigInteger b) const {
+	int alen = getlen(), blen = b.getlen();
+	int max = alen > blen ? alen : blen;
+	int carry = 0;
+	BigInteger result;
+	int i = 0;
+
+	while (max > 0 || carry) {
+		int sum = arr[NR_DIGITS-i-1] + b.arr[NR_DIGITS-i-1] + carry;
+		int dig = sum % 10;
+		carry = sum/10;
+		result.prependDigits(dig);
+		i++;
+		max--;
+	}
+
+	return result;
+}
+
+BigInteger BigInteger::operator -(BigInteger b) const {
+	BigInteger result;
+	int dig, carry = 0;
+	for (int i = NR_DIGITS - 1; i >= 0; i--) {
+		if ((arr[i] + carry) < b.getDigitAt(i)) {
+			dig = arr[i] + carry + 10 - b.getDigitAt(i);
+			carry = -1;
+		} else {
+			dig = arr[i] + carry - b.getDigitAt(i);
+			carry = 0;
+		}
+		result.prependDigits(dig);
+	}
+	result.shrink();
+	return result;
+}
+
+int main(int argc, char *argv[]) {
+	BigInteger b1(50);
+	BigInteger b2 = 100;
+	BigInteger b3 = b1 - b2;
+	b3.show();
+	return 0;
+}
